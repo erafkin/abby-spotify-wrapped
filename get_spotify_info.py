9a110b340f7898fc832d392013dfe11f7f31d033
Emma Rafkin <emmarafkin@gmail.com>
2024-12-13 13:39:22 -0500

    fixes for jessica'

--- get_spotify_info.py
+++ get_spotify_info.py
@@ -8,9 +8,8 @@ load_dotenv()
 scope = "user-library-read user-top-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
+file_prepend="./output/jessica_"
 time_range = "long_term"
-
 
 got_all_artists = False
 offset = 0
@@ -35,14 +34,14 @@ rows = zip(vals, counts)
 artist_genre_df = pd.DataFrame(rows,columns=["genre", "count"])
 
 artist_genre_df = artist_genre_df.sort_values(["count"], ascending=False)
-artist_genre_df.to_csv("./output/artist_genre_info.csv", index=False)
+artist_genre_df.to_csv(f"{file_prepend}artist_genre_info.csv", index=False)
 
 artist_info = []
 for artist in artist_ids:
     info = sp.artist(artist)
     artist_info.append({"artist": info["name"], "popularity": info["popularity"], "genres": info["genres"]})
 artist_df = pd.DataFrame(artist_info)
-artist_df.to_csv("./output/artist_info.csv", index=False)
+artist_df.to_csv(f"{file_prepend}artist_info.csv", index=False)
 
 got_all_songs = False
 offset = 0
@@ -63,11 +62,12 @@ while total_top_songs is None or  len(songs) < total_top_songs:
         songs.append(song)
     offset += len(fetch["items"])
 song_df = pd.DataFrame(songs)
-song_df.to_csv("./output/song_info.csv", index=False)
+song_df.to_csv(f"{file_prepend}song_info.csv", index=False)
 
-album_counts = pd.read_csv("./output/song_info.csv", index_col=0)
-album_counts = album_counts.drop('popularity', axis=1)
-album_counts = album_counts.groupby("album", sort=False).agg(lambda x: ', '.join(x.unique()))
-
-album_counts["count"] = song_df.album.value_counts()
-album_counts.to_csv("./output/album_info.csv", index=False)
+album_counts = pd.read_csv(f"{file_prepend}song_info.csv", index_col=0)
+album_counts = album_counts.drop('popularity', axis=1).reset_index()
+counts = album_counts["album"].value_counts()
+album_counts = album_counts.groupby("album", sort=False).agg(lambda x: ', '.join(x.unique())).reset_index()
+album_counts = pd.merge(album_counts, counts, on='album')
+album_counts = album_counts.drop('song', axis=1)
+album_counts.to_csv(f"{file_prepend}album_info.csv", index=False)
